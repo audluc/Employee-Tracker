3790d25fc3c44384716112bab5ce01d6c25d0956
Audra Dagenais <audluc44@gmail.com>
2020-10-10 15:35:44 -0700

    added view department, add role, view role

--- index.js
+++ index.js
@@ -36,7 +36,12 @@ function start() {
       switch (answers.options) {
         case "add department":
           return addDepartment();
-
+        case "view department":
+          return viewDepartment();
+        case "add role":
+          return addRole();
+          case "view roles":
+            return viewRole();  
         default:
           return process.exit();
       }
@@ -52,14 +57,67 @@ function addDepartment() {
       },
     ])
     .then(function (answers) {
-        console.log(answers)
-      connection.query("INSERT INTO department SET ?", {name: answers.name}, function (
-        error,
-        result
-      ) {
-        if (error) throw error;
-        console.log ("department created")
-        connection.end()
-      });
+      console.log(answers);
+      connection.query(
+        "INSERT INTO department SET ?",
+        { name: answers.name },
+        function (error, result) {
+          if (error) throw error;
+          console.log("department created");
+          connection.end();
+        }
+      );
     });
+}
+function viewDepartment() {
+  connection.query("SELECT * FROM department", function (error, result) {
+    if (error) throw error;
+    console.table(result);
+    connection.end();
+  });
+}
+function addRole() {
+  connection.query("SELECT * FROM department", function (error, result) {
+    if (error) throw error;
+    const departments = result.map(function (dep) {
+      return { value: dep.id, name: dep.name };
+    });
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is your title",
+          name: "title",
+        },
+        {
+          type: "input",
+          message: "What is your salary",
+          name: "salary",
+        },
+        {
+          type: "list",
+          choices: departments,
+          message: "What is your department ID?",
+          name: "department_id",
+        },
+      ])
+      .then(function (answers) {
+        console.log(answers);
+        connection.query("INSERT INTO role SET ?", answers, function (
+          error,
+          result
+        ) {
+          if (error) throw error;
+          console.log("role created");
+          connection.end();
+        });
+      });
+  });
+}
+function viewRole() {
+  connection.query("SELECT * FROM role LEFT JOIN department ON role.department_id = department.id", function (error, result) {
+    if (error) throw error;
+    console.table(result);
+    connection.end();
+  });
 }
