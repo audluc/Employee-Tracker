e4ee6993522b206c68fc40bdf46059d4d2397aad
Audra Dagenais <audluc44@gmail.com>
2020-10-17 15:38:44 -0700

    added seeds.sql

--- Schema.sql
+++ Schema.sql
@@ -14,13 +14,13 @@ CREATE TABLE role(
 );
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) UNIQUE NOT NULL,
-  last_name VARCHAR(30) UNIQUE NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  FOREIGN KEY('role_id') REFERENCES role('id'),
-  FOREIGN KEY('manager_id') REFERENCES employee('id') 
-  
-  <!---CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  <!---manager_id INT,
-  <!---CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+
+    --NULL FOREIGN KEY('role_id') REFERENCES role('id'),
+    --FOREIGN KEY('manager_id') REFERENCES employee('id')
 );
